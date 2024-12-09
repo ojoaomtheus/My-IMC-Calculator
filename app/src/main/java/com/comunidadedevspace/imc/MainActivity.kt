0c3faa5508bbffb9f474144c3c5ab17d249a435a
ojoaomtheus <joao.silva0754@gmail.com>
2024-12-09 09:24:49 +0000

    Calculo do IMC

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,33 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Preciso conectar minha mainActivity com o xml
+
+        val edtPeso = findViewById<TextInputEditText>(R.id.idt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.idt_altura)
+
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+            val peso: Float = edtPeso.text.toString().toFloat()
+            val altura: Float = edtAltura.text.toString().toFloat()
+
+            val alturaQ2 = altura * altura
+            val resultado = peso / alturaQ2
+
+            println("joao" + resultado)
+        }
+
     }
 }
+
+
+
